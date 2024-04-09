0b713ab4222c20d7dd3dab449329d28d36c8e469
Vasanth <kanva001@gmail.com>
2024-04-09 15:02:28 -0400

    updates

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ List_of_files=[
      "src/utils/__init__.py",
      "src/utils/utils.py",
      "src/logger/logging.py",
-     "src/exception/exception",
+     "src/exception/exception.py",
      "tests/unit/__init__.py",
      "tests/integration/__init__.py",
      "init_setup.sh",
